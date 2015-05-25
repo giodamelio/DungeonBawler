dcf8125e35dbaf52be927880045579d862365e33
Gio d'Amelio <giodamelio@gmail.com>
2015-05-24 22:26:22 -0700

    Add quit button

--- src/main/mainWindow.js
+++ src/main/mainWindow.js
@@ -3,7 +3,10 @@
 import {EventEmitter} from 'events';
 import path from 'path';
 
+import app from 'app';
 import BrowserWindow from 'browser-window';
+import Menu from 'menu';
+import MenuItem from 'menu-item';
 
 class MainWindow extends EventEmitter {
   constructor() {
@@ -14,12 +17,31 @@ class MainWindow extends EventEmitter {
       height: 600
     });
 
+    // Load the html file
     let indexFile = 'file://' + path.resolve(__dirname, '../browser/index.html');
     this.window.loadUrl(indexFile);
 
+    // Send close event
     this.window.on('closed', () => {
       this.emit.apply(this, arguments);
     });
+
+    // Create context menu
+    this.menu = Menu.buildFromTemplate([
+      {
+        label: 'File',
+        submenu: [
+          {
+            label: 'Exit',
+            accelerator: 'CommandOrControl+q',
+            click() {
+              app.quit();
+            }
+          }
+        ]
+      }
+    ]);
+    Menu.setApplicationMenu(this.menu);
   }
 }
 
