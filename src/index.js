98f6e1b6295012827b179bd0c680f520f5ad98c8
Gio d'Amelio <giodamelio@gmail.com>
2015-05-27 22:20:54 -0700

    React hello world

--- src/index.js
+++ src/index.js
@@ -1,2 +1,15 @@
-console.log(`1 + 1 = ${1 + 1}`);
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return <h1>Hello React!</h1>;
+  }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  React.render(
+    <App></App>,
+    document.getElementById('container')
+  );
+});
 
