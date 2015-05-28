d9e839c4eda1a40cfbd842b7756e8eedc46c1571
Gio d'Amelio <giodamelio@gmail.com>
2015-05-27 18:19:36 -0700

    Fresh start

--- src/browser/index.js
+++
@@ -1,9 +0,0 @@
-console.log('Starting App');
-
-// Log messages from child webviews
-Array.from(document.getElementsByTagName('webview')).forEach(function(webview) {
-  webview.addEventListener('console-message', function(e) {
-    console.log('Webview logged:', e.message);
-  });
-});
-
